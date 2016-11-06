8b2e9f7efc3758b6b3558f0e286062ab715e0f18
northPQ <colurfulcat13@gmail.com>
2016-11-06 22:06:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, ' ', { preload: preload, create: create, update: update });
 function preload() {
-    game.load.image('sky', 'sky.png');
-    game.load.image('ground', 'platform.png');
-    game.load.image('star', 'star.png');
-    game.load.spritesheet('dude', 'dude.png', 32, 48);
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 var platforms;
 var player;
